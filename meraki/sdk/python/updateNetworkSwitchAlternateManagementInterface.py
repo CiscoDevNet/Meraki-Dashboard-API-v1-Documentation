700b41c494cd9099e3db423d856ea9bdbae71682
John M. Kuchta <git@chir.us>
2021-02-26 15:26:55 -0800

    Updated for SDK v1.6.2
    Updated for SDK v1.6.2

---
+++ meraki/sdk/python/updateNetworkSwitchAlternateManagementInterface.py
@@ -0,0 +1,20 @@
+import meraki
+
+# Defining your API key as a variable in source code is not recommended
+API_KEY = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
+# Instead, use an environment variable as shown under the Usage section
+# @ https://github.com/meraki/dashboard-api-python/
+
+dashboard = meraki.DashboardAPI(API_KEY)
+
+network_id = 'L_646829496481105433'
+
+response = dashboard.switch.updateNetworkSwitchAlternateManagementInterface(
+    network_id, 
+    enabled=True, 
+    vlanId=100, 
+    protocols=['radius', 'snmp', 'syslog'], 
+    switches=[{'serial': 'Q234-ABCD-5678', 'alternateManagementIp': '1.2.3.4'}]
+)
+
+print(response)
