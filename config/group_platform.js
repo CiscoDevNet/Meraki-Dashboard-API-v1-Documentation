c6f203141c5343820d81e2ac6e7b305dd1e6541c
dexterlabora <cguynn@gmail.com>
2022-08-04 11:17:16 +0200

    add platform groupBy and early access filterBy

---
+++ config/group_platform.js
@@ -0,0 +1,101 @@
+
+   
+
+// Order by Tags array
+
+//  "tags": [
+//         "appliance",
+//         "configure",
+//         "uplinks",
+//         "settings"
+//  ],
+//  "x-release-stage": "beta"
+
+// domain/category/serviceGroup/serviceGroup
+
+
+const groupBy = function (operation, meta) {
+    let path = ""
+    let tags = [...[],...operation.tags]
+   // let releaseStage = operation["x-release-stage"] || ""
+    let scopes = ['organizations', 'networks', 'devices','administered'];
+    if(scopes.find(s => tags[0] === s)){
+        path = "Platform/";
+    }else{
+        path = "Products/";
+    }
+    
+    // if(releaseStage){
+    // 	// add release stage to end of tags
+    // 	tags.push(releaseStage)
+       
+    //   // add nav tree root
+    //   if(releaseStage === "beta"){
+    //         path = "_Early API Access_/" + path
+    //     }else{
+    //         path = releaseStage.toUpperCase() + "/" + path
+    //     }
+    // }
+    
+    
+   
+        
+    // Build Folder Tree
+    console.log('tags',tags)
+    let tLength = tags.length
+    tags.forEach((t,i) => {
+      
+        if(i == 1 && t === 'liveTools' && tags[0] === 'devices'){
+            path += t + "/devices/";
+        }
+        if(i === 1 && tags[0] === 'administered'){
+            path += t + "/administered/";
+        }
+        // if(i == 0 && scopes.find(s => tags[i] === s && s !== 'administered' )){
+        //     // skip scope
+        // }
+        else if(i == 0 && scopes.find(s => tags[i] === s)){
+            // skip scope
+        }
+        // else if( i == 1 && t !== "liveTools"){
+        //     // skip category
+        
+        // }
+        // else if(t === releaseStage){
+        //     // skip release
+        // }
+        else{
+            
+            // build path
+            path += t + "/";
+        }
+        
+    });
+    
+    path = path.replace(/\/$/, "");
+
+ 
+    return path;
+}
+
+module.exports = groupBy;
+
+// ********************
+// testing
+// ********************
+
+
+let productTest = {
+    "x-release-stage":"beta",
+    tags: [ 'switch', 'monitor', 'devices', 'ports', 'statuses']
+}
+let scopeTest = {
+    tags: [ 'networks', 'configure', 'floorPlans']
+}
+
+
+console.log("productTest", productTest, groupBy(productTest));
+// PRODUCTS/switch/MONITOR/devices/ports
+
+//console.log("scopeTest", scopeTest, groupBy(scopeTest));
+// GENERAL/networks/CONFIGURE/floorPlans
