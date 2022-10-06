ed959a12b5a2126d2d706237d1f9db93227154b9
Dexter LaBora <cguynn@gmail.com>
2022-10-06 13:08:23 +0200

    v1.26.0 and changed demo environment

--- config/api_key.js
+++ config/api_key.js
@@ -1,3 +1,4 @@
 module.exports = async function(){
-    return "6bec40cf957de430a6f1f2baa056b99a4fac9ea0";
+    //return "6bec40cf957de430a6f1f2baa056b99a4fac9ea0";
+    return "75dd5334bef4d2bc96f26138c163c0a3fa0b5ca6";
 }
